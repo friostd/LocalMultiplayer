bf41c3228bc7805049a83c11d825fec9eb717e96
FrioGitHub <87456223+FrioGitHub@users.noreply.github.com>
2021-12-10 16:28:15 -0300

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -1,39 +1,39 @@
 package JAVARuntime;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.lang.Thread;
 
 public class Servidor extends Thread {
+	
+	private static Socket cliente;
+	public static int porta = 2020;
+	
+	private static ServerSocket servidor = null;
+	
+	public static void main(String[] args) {
+		try {
+			servidor = new ServerSocket(porta);
+			System.out.println("Servidor ligado");
+			
+			while (true) {
+				Socket cliente = servidor.accept();
+				Thread t = new Servidor(cliente);
+				t.start();
+				System.out.println("Cliente conectado");
+			}
+		}
+		catch (IOException ex) {
+			System.out.println("Erro " + ex);
+		}
+	}
     
-    private static Socket cliente;
-    public static int porta = 2029;
-    
-    private static ServerSocket servidor = null;
-    
-    public static void main(String[] args) {
-        try {
-            servidor = new ServerSocket(porta);
-            System.out.println("Servidor ligado");
-            
-            while (true) {
-                Socket cliente = servidor.accept();
-                Thread t = new Servidor(cliente);
-                t.start();
-                System.out.println("Cliente conectado");
-            }
-        }
-        catch (IOException ex) {
-            System.out.println("Erro " + ex);
-        }
-    }
-    
-    public Servidor(Socket servidor) {
-        cliente = servidor;
-    }
-    
-    public void run() {
-        
-    }
-    
+	public Servidor(Socket servidor) {
+		cliente = servidor;
+	}
+	
+	public void run() {
+		
+	}
 }
