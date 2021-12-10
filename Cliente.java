a22335606c743b10e0df6ff5f2a7eec6be7cfc5c
FrioGitHub <87456223+FrioGitHub@users.noreply.github.com>
2021-12-10 16:26:46 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,28 +1,24 @@
 package JAVARuntime;
 
-import java.util.*;
-import java.net.*;
-import java.io.*;
+import java.net.Socket;
+import java.io.IOException;
 
-public class Cliente extends Component { 
-
-    public static Socket cliente;
-    public int porta;
-    
-
-    public void start() {
-        try {
-        	    cliente = new Socket("localhost", porta);
-        	    Console.log("Servidor Conectado");
-        	}
-        	catch(IOException ex){
-        	    Console.log("Erro " + ex);
-        	}
-        
-    }
-
-    public void repeat() {
-        
-    }
-
+public class Cliente extends Component {
+	
+	public static Socket cliente;
+	public int porta = 2020;
+	
+	public void start() {
+		try {
+			cliente = new Socket("localhost", porta);
+			Console.log("Servidor Conectado");
+		}
+		catch(IOException ex){
+			Console.log("Erro " + ex);
+		}
+	}
+	
+	public void repeat() {
+		
+	}
 }
